a45667b7ec6cf1e540edc1fc990aa372b1cb3f85
cshenton <charlieshenton@me.com>
2016-06-07 12:05:18 +1000

    Reshaped data to id-month long

--- dataClean.R
+++ dataClean.R
@@ -1,12 +1,47 @@
-
+library(reshape)
+library(dplyr)
 
 setwd("/Users/charlesshenton/Documents/LazyLearning/ProductSales")
 
 
-# Load Data 
+# Load Data
 train <- read.csv('TrainingDataset.csv',
 			header = T, stringsAsFactors = F)
 test <- read.csv('TestDataset.csv',
 			header=TRUE, stringsAsFactors = F)
 
-# Check for Missing Data
+# Combine data for Cleaning
+target <- c("Outcome_M1",
+			"Outcome_M2",
+			"Outcome_M3",
+			"Outcome_M4",
+			"Outcome_M5",
+			"Outcome_M6",
+			"Outcome_M7",
+			"Outcome_M8",
+			"Outcome_M9",
+			"Outcome_M10",
+			"Outcome_M11",
+			"Outcome_M12")
+test[target] <- NA
+testIDs <- test$id
+test$id <- NULL 			
+data <- rbind(train, test)
+
+# Reshape to long (id-month)
+dataLong <- melt(data, measure = target)
+dataLong$month <- dataLong$variable %>%
+	as.character() %>%
+	substring(10, nchar(.)) %>%
+	as.integer()
+dataLong$target <- dataLong$value
+dataLong$value <- NULL
+dataLong$variable <- NULL
+
+# Check for, encode Missing Data
+missing <- sapply(train, function(x) sum(is.na(x)))
+hasMissing <- names(missing[missing > 0])
+
+for(name in hasMissing) {
+	t
+}
